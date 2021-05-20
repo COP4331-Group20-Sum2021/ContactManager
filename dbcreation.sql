4065c846217cbe8c1eb57541b1a37f3e02a422c9
Nate8888 <wilk.nathan@gmail.com>
2021-05-20 19:59:42 -0400

    mySQL setup

---
+++ dbcreation.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE ContactManager;
+
+CREATE TABLE `ContactManager`.`users`
+  (
+     `id`               INT NOT NULL auto_increment,
+     `datecreated`      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     `datelastloggedin` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     `firstname`        VARCHAR(50) NOT NULL DEFAULT '',
+     `lastname`         VARCHAR(50) NOT NULL DEFAULT '',
+     `login`            VARCHAR(50) NOT NULL DEFAULT '',
+     `password`         VARCHAR(50) NOT NULL DEFAULT '',
+     PRIMARY KEY (`id`)
+  )
+engine = innodb;
+
+CREATE TABLE `ContactManager`.`contacts`
+  (
+     `id`     INT NOT NULL auto_increment,
+     `firstname`   VARCHAR(50) NOT NULL DEFAULT '',
+	 `lastname`   VARCHAR(50) NOT NULL DEFAULT '',
+     `userid` INT NOT NULL DEFAULT '0',
+	 `phone` VARCHAR(50) NOT NULL DEFAULT '',
+	 `email` VARCHAR(50) NOT NULL DEFAULT '',
+	 `description` VARCHAR(100) NOT NULL DEFAULT '',
+	 `pic` BLOB,
+     PRIMARY KEY (`id`)
+  )
+engine = innodb; 
---
+++ insertion.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (firstname,lastname,login,password) VALUES ('Nate','Wilk','N8','abc123');
+INSERT INTO users (firstname,lastname,login,password) VALUES ('John','Doe','jdoe','abc123');
+INSERT INTO users (firstname,lastname,login,password) VALUES ('Cool','Joe','cjoe','abc123');
+
+INSERT INTO contacts (firstname,lastname,userid,phone,email,description,pic) VALUES ('Cool','Guy',1,'999999999','cool@cool.com','dude I met at the park', null);
+INSERT INTO contacts (firstname,lastname,userid,phone,email,description,pic) VALUES ('Ice','Cream',1,'888888888','ice@cream.com','Free ice cream treats', null);
+INSERT INTO contacts (firstname,lastname,userid,phone,email,description,pic) VALUES ('BBQ','Guy',1,'777777777','BBQ@guy.com','BBQ Social', null);
+INSERT INTO contacts (firstname,lastname,userid,phone,email,description,pic) VALUES ('Cool','Guy',2,'999999999','cool@cool.com','dude I met at the park', null);
+INSERT INTO contacts (firstname,lastname,userid,phone,email,description,pic) VALUES ('Cool','Guy',3,'999999999','cool@cool.com','dude I met at the park', null);
