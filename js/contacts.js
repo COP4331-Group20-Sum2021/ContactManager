729ee9cd3a44b62b7f979868be1db9bdc0beffaf
Devin Besaw <devinbesaw15@yahoo.com>
2021-05-25 01:54:25 -0400

    Added initials for contact profile picture

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,24 @@
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+var firstInitial;
+var lastInitial;
+
+firstName.addEventListener('keyup', (e) =>
+{
+    const input = e.target.value;
+    firstInitial = input.charAt(0);
+    
+});
+
+lastName.addEventListener('keyup', (f) =>
+{
+    const input2 = f.target.value;
+    lastInitial = input2.charAt(0);
+    const fl = firstInitial + lastInitial;
+
+    document.getElementById("fl").innerHTML = fl;
+});
+
 document.getElementById('button').addEventListener('click', 
 function()
 {
