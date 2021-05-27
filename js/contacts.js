ee3ee7e87021c4a77afed56b279f5264b92d4d60
Devin Besaw <devinbesaw15@yahoo.com>
2021-05-26 23:50:23 -0400

    Confirm delete contact popup

--- js/contacts.js
+++ js/contacts.js
@@ -9,6 +9,7 @@ const phone = document.getElementById('phone');
 
 const getPlusButton = document.getElementById('plusButton');
 const getCloseButton = document.querySelector('.close');
+const getConfirmCloseButton = document.querySelector('.confirm-delete-close');
 
 const getDoneButton = document.querySelector('.done-button');
 const contactsTable = document.querySelector('.contacts-body');
@@ -25,6 +26,9 @@ phone.addEventListener('keyup', getPhone);
 getPlusButton.addEventListener('click', plusButton);
 getDoneButton.addEventListener('click', addContact);
 getCloseButton.addEventListener('click', closeButton);
+getConfirmCloseButton.addEventListener('click', closeConfirmButton);
+
+
 contactsTable.addEventListener('click', deleteRow);
 
 // =============================================================================
@@ -88,6 +92,12 @@ function plusButton()
 function closeButton()
 {
     document.querySelector('.bg-modal').style.display = 'none';
+}
+
+// close add contact popup with 'x' button
+function closeConfirmButton()
+{
+    document.querySelector('.bg-modal-confirm-delete').style.display = 'none';
 }
 
 // add contact
@@ -156,8 +166,14 @@ function deleteRow(e)
 
     if (b.classList[0] === 'delete-button')
     {
-        const contactCell = b.parentElement;
-        const contactRow = contactCell.parentElement;
-        contactRow.remove();
+        document.querySelector('.bg-modal-confirm-delete').style.display = 'flex';
+
+        document.querySelector('.confirm-button').onclick = function()
+        {
+            document.querySelector('.bg-modal-confirm-delete').style.display = 'none';
+            const contactCell = b.parentElement;
+            const contactRow = contactCell.parentElement;
+            contactRow.remove();
+        }
     }
 }
