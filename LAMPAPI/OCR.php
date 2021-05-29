6501d8ca79e7ad392f0ef1ff48e59177a7043fae
Nate8888 <wilk.nathan@gmail.com>
2021-05-28 20:09:40 -0400

    Changed OCR feature to handle api requests instead of form submission

--- LAMPAPI/OCR.php
+++ LAMPAPI/OCR.php
@@ -1,68 +1,62 @@
 <?php
+
   require 'vendor/autoload.php';
-  
+
   use Google\Cloud\Vision\V1\Feature\Type;
   use Google\Cloud\Vision\V1\ImageAnnotatorClient;
   use Google\Cloud\Vision\V1\Image;
   
   putenv('GOOGLE_APPLICATION_CREDENTIALS=/cred.json');
-
+  
   function process($file){
-  $client = new ImageAnnotatorClient();
-  
-  // Annotate an image, detecting faces.
-  $annotation = $client->annotateImage(
-      $file,
-      [Type::TEXT_DETECTION]
-  );
-  
-  
-  $fullText = $annotation->getFullTextAnnotation()->getText();
-  return $fullText;
-  
-  }
+        $client = new ImageAnnotatorClient();
 
-  $target_dir = "uploads/";
-  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-  //echo "$target_file";
-  $uploadOk = 1;
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-  // Check if image file is a actual image or fake image
-  
-  if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-      echo "File is an image - " . $check["mime"] . ".<br><br>\r\n\r\n";
-      $uploadOk = 1;
-    } else {
-      echo "File is not an image.";
-      $uploadOk = 0;
-    }
-  }
-  
-  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-  && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br><br>\n";
-    $uploadOk = 0;
-  }
-  
-  // Check if $uploadOk is set to 0 by an error
-  if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-  // if everything is ok, try to upload file
-  } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-      $file = fopen($target_file, 'r');
-      $complete = process($file);
-      echo "Text Found in the image: <br><br> $complete";
-      $arr = explode("\n", $complete);
-      //var_dump($arr);
-      echo "<br><br>The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.\n";
-      
-    } else {
-      echo "Sorry, there was an error uploading your file.\n";
-    }
-  }
+        // Annotate an image, detecting text.
+        $annotation = $client->annotateImage(
+            $file,
+            [Type::TEXT_DETECTION]
+        );
 
 
+        $fullText = $annotation->getFullTextAnnotation()->getText();
+        return $fullText;
+  }
+    
+  if(isset($_FILES['file']['name'])){
+     // file name
+     $filename = $_FILES['file']['name'];
+  
+     // Location
+     $location = 'uploads/'.$filename;
+  
+     // file extension
+     $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+     $file_extension = strtolower($file_extension);
+  
+     // Valid extensions
+     $valid_ext = array("pdf","doc","docx","jpg","png","jpeg");
+     //echo $location;
+     $response = 0;
+     if(in_array($file_extension,$valid_ext)){
+        // Upload file
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $location)){
+            $file = fopen($location, 'r');
+            $complete = process($file);
+            //echo $complete;
+            //$arr = explode("\n", $complete);
+            returnWithInfo($complete);
+        } 
+     }
+  }
+  
+  function returnWithInfo($text) {
+    $retValue = '{"text":' . $text . '}';
+    sendResultInfoAsJson($retValue);
+  }
+  function sendResultInfoAsJson($obj) {
+    header('Content-type: application/json');
+    echo $obj;
+  }
+
+  
 ?>
