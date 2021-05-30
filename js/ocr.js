cead8ac1f61768ddd92db981139ac51764fd91cc
Nate8888 <wilk.nathan@gmail.com>
2021-05-30 17:42:03 -0400

    Added Filters for fname, lname, email, phone on OCR feature

--- js/ocr.js
+++ js/ocr.js
@@ -1,6 +1,6 @@
 function uploadFile() {
     var files = document.getElementById("file").files;
-        
+    
     if(files.length > 0 ){
         
 		var formData = new FormData();
@@ -15,7 +15,13 @@ function uploadFile() {
         xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var response = this.responseText;
-            console.log(response);
+			
+			// Holds an array with the text scanned.
+			var myObj = JSON.parse(response);
+			
+			// Creates an object with potential contactData
+			var processedResults = createContactObject(myObj);
+			console.log(processedResults);
             }
         };
         
@@ -28,5 +34,65 @@ function uploadFile() {
             alert("Please select a file");
         }
         
-    }
+}
+
+function createContactObject(textArray) {
+	
+	var fname = "";
+	var lname = "";
+	var phone = "";
+	var email = "";
+	var descr = "Scanned with OCR: ";
+	
+	// Try to use the scanned info to build the userdata.
+	for (let i = 0; i < textArray.length; i++){
+		
+		if (isPhonenumber(textArray[i])){
+			phone = textArray[i];
+			continue;
+		}
+		
+		if (isName(textArray[i])){
+			fname = textArray[i].split(" ")[0];
+			lname = textArray[i].split(" ")[1];
+			continue;
+		}
+		
+		if (isEmail(textArray[i])){
+			email = textArray[i];
+			continue;
+		}
+		// If it's not any of them, we just throw it here for the user to sort it.
+		descr += textArray[i] + "\n";
+		
+	}
+	
+	var obj = {fname: fname, lname: lname, phone: phone, email: email, descr: descr};
+	return obj;
+
+}
+
+function isPhonenumber(currentString){
+	
+	let amtDigits = 0;
+	for (let i = 0; i < currentString.length; i++){
+		let currentChar = currentString.charAt(i);
+		if (currentChar >= '0' && currentChar <= '9') {
+			amtDigits++;
+		}
+	}
+	// Let's say we got a partial phone lol, I'll consider it as a phone number.
+	if (amtDigits >= 6)
+		return true;
+	return false;
+	
+}
+
+function isName(currentString){
+	return currentString.split(" ").length > 1 && !(currentString.includes("@"));
+}
+
+function isEmail(currentString){
+	return currentString.includes("@");
+}
 
