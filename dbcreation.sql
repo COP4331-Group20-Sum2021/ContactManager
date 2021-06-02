f1e29050e990de6c6283b38280d560aae0970914
Nathan Kurelo Wilk <40876504+Nate8888@users.noreply.github.com>
2021-06-02 18:05:05 -0400

    Add lastupdate field to the table

--- dbcreation.sql
+++ dbcreation.sql
@@ -17,12 +17,13 @@ CREATE TABLE `ContactManager`.`contacts`
   (
      `id`     INT NOT NULL auto_increment,
      `firstname`   VARCHAR(50) NOT NULL DEFAULT '',
-	 `lastname`   VARCHAR(50) NOT NULL DEFAULT '',
+     `lastname`   VARCHAR(50) NOT NULL DEFAULT '',
      `userid` INT NOT NULL DEFAULT '0',
-	 `phone` VARCHAR(50) NOT NULL DEFAULT '',
-	 `email` VARCHAR(50) NOT NULL DEFAULT '',
-	 `description` VARCHAR(100) NOT NULL DEFAULT '',
+     `phone` VARCHAR(50) NOT NULL DEFAULT '',
+     `email` VARCHAR(50) NOT NULL DEFAULT '',
+     `description` VARCHAR(100) NOT NULL DEFAULT '',
+     `lastupdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
      PRIMARY KEY (`id`),
-	 FOREIGN KEY (userid) REFERENCES users(id)
+     FOREIGN KEY (userid) REFERENCES users(id)
   )
 engine = innodb; 
